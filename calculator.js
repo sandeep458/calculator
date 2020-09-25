e61a66c166d3934f311e046805cb56d672a23a52
sandeep458 <63731398+sandeep458@users.noreply.github.com>
2020-09-25 14:53:09 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,102 @@
+import React from 'react'; 
+import CalculatorTitle from './calculatorTitle.js'; 
+import OutputScreen from './outputScreen.js'; 
+import Button from './button.js'; 
+  
+class Calculator extends React.Component { 
+    constructor() { 
+        super(); 
+      this.state = { 
+          question: '', 
+          answer: ''
+        } 
+       this.handleClick = this.handleClick.bind(this); 
+      } 
+      handleClick(event){ 
+ const value = event.target.value; 
+       switch (value) { 
+          case '=': { 
+
+       if (this.state.question!=='') 
+            { 
+                var ans=''; 
+                  try
+                    {
+                        // eslint-disable-next-line 
+                        ans = eval(this.state.question); 
+                    } 
+                    catch(err) 
+                    { 
+                        this.setState({answer: "Math Error"}); 
+                    } 
+                    if (ans===undefined) 
+                        this.setState({answer: "Math Error"}); 
+       
+                    // update answer in our state. 
+                    else
+                        this.setState({ answer: ans , question: ''}); 
+                    break; 
+                } 
+          } 
+          break;
+          case 'Clear': { 
+        this.setState({ question: '', answer: '' }); 
+            break; 
+          } 
+       
+          case 'Delete': { 
+            var str = this.state.question; 
+              str = str.substr(0,str.length-1); 
+              this.setState({question: str}); 
+              break; 
+          } 
+       
+        default: { 
+       this.setState({ question: this.setstate.question += value}) 
+            break; 
+          } 
+        } 
+      } 
+
+
+    render() 
+    { 
+    return ( 
+    <div className="frame"> 
+    <CalculatorTitle value="simple Calculator"/> 
+    <div class="mainCalc"> 
+    <OutputScreen/> 
+    <div className="button-row"> 
+      <Button label={'Clear'}/> 
+      <Button label={'Delete'}/> 
+      <Button label={'.'}/> 
+      <Button label={'/'}/> 
+    </div> 
+    <div className="button-row"> 
+      <Button label={'7'}/> 
+      <Button label={'8'}/> 
+      <Button label={'9'}/> 
+      <Button label={'*'}/> 
+    </div> 
+    <div className="button-row"> 
+      <Button label={'4'}/> 
+      <Button label={'5'}/> 
+      <Button label={'6'}/> 
+      <Button label={'-'}/> 
+    </div> 
+    <div className="button-row"> 
+      <Button label={'1'}/> 
+      <Button label={'2'}/> 
+      <Button label={'3'}/> 
+      <Button label={'+'}/> 
+    </div> 
+    <div className="button-row"> 
+      <Button label={'0'}/> 
+      <Button label={'='}/> 
+    </div> 
+    </div> 
+    </div> 
+    ); 
+  } 
+} 
+  export default Calculator; 
---
+++ index.js
@@ -0,0 +1,4 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import Calculator from './components/calculator.js'; 
+ReactDOM.render(<Calculator />, document.getElementById('root'));
